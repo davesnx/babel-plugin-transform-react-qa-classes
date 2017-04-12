58ab49917984f2be4e564ceee71cabb822e6e412
David Sancho <dsnxmoreno@gmail.com>
2017-04-12 19:22:39 +0200

    feat(init): Create basic plugin boilerplate and mocha test suit

---
+++ test/fixtures/example/expected.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class componentName extends Component {
+  render () {
+    return (
+      <div classNames='componentName'>
+        <h1>Hello world</h1>
+      </div>
+    )
+  }
+}
+
+export default componentName
