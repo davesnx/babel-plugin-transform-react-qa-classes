f175f38402940a314fc2ed8f78fa387410b24ee5
David Sancho <dsnxmoreno@gmail.com>
2017-04-16 15:47:41 +0200

    test(arrow): Add arrow function component test case

---
+++ test/fixtures/react/arrow/expected.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const componentName = () => {
+  return <div data-qa="componentname">
+    <h1>Hello world</h1>
+  </div>;
+}
+
+export default componentName;
