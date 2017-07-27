2062b875ade416643ca8f0da5e65912ab58a33fc
Tanapol Nearunchorn <tanapoln@wongnai.com>
2017-07-27 19:35:21 +0700

    update dependencies, fix error on plain class, add tests

---
+++ test/fixtures/react/pureComponent/expected.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PureComponentName extends React.PureComponent {
+    render() {
+        return <div data-qa="pure-component-name">
+            <h1>Hello world</h1>
+        </div>;
+    }
+}
+
+export default PureComponentName;
